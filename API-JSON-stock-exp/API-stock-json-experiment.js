4eedeaab5257f3c8f3198cfb1dd855f7a5efbdff
Kerry Bodle <kerrybodle@yahoo.com>
2018-10-13 23:02:51 -0400

    Add files via upload

--- API-JSON-stock-exp/API-stock-json-experiment.js
+++ API-JSON-stock-exp/API-stock-json-experiment.js
@@ -1,14 +1,15 @@
 var url = "http://api.open-notify.org/astros.json"; // Change this to your URL
+
+
+
 fetch(url)
     .then(function(response) {
         if (response.ok) { // Check if response went through
             response.json().then(function(data) {
                 var info = document.getElementById('info');
-                info.innerHTML;
+
             });
         } else { // Response wasn't ok. Check dev tools
             console.log("response failed?");
         }
     });
-
-document.querySelector("info");
